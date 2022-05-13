299f924db546b89dc65e28ab916ed7d5aec43082
Ruslan Tikhonov <tihoo99@icloud.com>
2022-05-13 14:55:53 +1000

    add landing build

---
+++ build/precache-manifest.e3a9ebf633f702c9e61ce38d6b644d07.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3bfc3fd5c7c5a9c7c13",
+    "url": "/static/css/main.70f71fd0.chunk.css"
+  },
+  {
+    "revision": "d3bfc3fd5c7c5a9c7c13",
+    "url": "/static/js/main.d3bfc3fd.chunk.js"
+  },
+  {
+    "revision": "fe38f0e4b790ffe19c69",
+    "url": "/static/css/1.96ee3432.chunk.css"
+  },
+  {
+    "revision": "fe38f0e4b790ffe19c69",
+    "url": "/static/js/1.fe38f0e4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b992319e2d525cb67d6637996663716",
+    "url": "/static/media/matcap-porcelain-white.1b992319.jpg"
+  },
+  {
+    "revision": "59382953e963fc41682a15fb8e005141",
+    "url": "/static/media/undershadow.59382953.png"
+  },
+  {
+    "revision": "efe6e944fdade340617edddcaef10090",
+    "url": "/static/media/rotate-left.efe6e944.svg"
+  },
+  {
+    "revision": "fc9255727f8a1f6212ea96e67f1abec3",
+    "url": "/static/media/rotate-right.fc925572.svg"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "98f0a55041ea0029c53ea7325f25e39d",
+    "url": "/index.html"
+  }
+];
